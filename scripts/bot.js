d56b23dfc1331359cbd182368dc509d2b8d629f4
Ras <asymptote.k.k@gmail.com>
2020-07-25 03:18:26 +0900

    NOTkanzenittiNOTkanzenitti

--- scripts/bot.js
+++ scripts/bot.js
@@ -2,26 +2,34 @@
 
 module.exports = robot => {
     // "@botName hoge"を受け取ったら"@senderName fuga"を送り返す
-    robot.respond(/hoge$/i, res => {
+    robot.respond(/.*hoge.*$/i, res => {
         res.reply("huga");
     });
 
-    robot.respond(/やんね！$/i, res => {
+    robot.respond(/.*やんね！.*$/i, res => {
         res.reply("やんね！");
     });
 
-    robot.respond(/もちもち$/i, res => {
+    robot.respond(/.*もちもち.*$/i, res => {
         res.reply("きなこもち～～～～～！");
     });
 
-    robot.respond(/きなこもち$/i, res => {
+    robot.respond(/.*きなこもち.*$/i, res => {
         res.send(":kinako.ex-large:");
     });
+
 
     robot.hear(/.*もふもふ.*$/i, res => {
         const ID = res.message.message.channelId;
         if(ID == "f58c72a4-14f0-423c-9259-dbb4a90ca35f"||ID == "159b8151-3f51-4c2d-857c-032aa4cc78e5"){
             res.send("もちもち～:blobenjoy:");
+        }
+    });
+
+    robot.hear(/.*寝.*$/i, res => {
+        const ID = res.message.message.channelId;
+        if(ID == "f58c72a4-14f0-423c-9259-dbb4a90ca35f"||ID == "159b8151-3f51-4c2d-857c-032aa4cc78e5"){
+            res.send("おやすみやんね！");
         }
     });
 
