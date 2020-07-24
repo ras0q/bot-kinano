324072cf478f40d1248a5d7244350fdccd1b3aa4
Ras <asymptote.k.k@gmail.com>
2020-07-25 00:37:57 +0900

    mochi2

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,6 +1,18 @@
 module.exports = robot => {
     // "@botName hoge"を受け取ったら"@senderName fuga"を送り返す
-    robot.respond(/もちもち$/i, res => {
-        res.reply("きなこもち～～～～！！！！！");
+    robot.respond(/hoge$/i, res => {
+        res.reply("huga");
     });
 };
+robot.respond(/もちもち$/i, reply => {
+    let https = require('https');
+    const URL = 'https://gentle-beach-41247.herokuapp.com/fukuokadam';
+    https.get(URL, (res) => {
+        res.on('data', (d) => {
+            console.log(d);
+            reply.reply((new TextDecoder).decode(d));
+    });
+    }).on('error', (e) => {
+    reply.reply(e);
+});
+});
