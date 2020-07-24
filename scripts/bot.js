58044e3a02f403f8def6ca166b2e83b48852a7d9
Ras <asymptote.k.k@gmail.com>
2020-07-25 01:51:38 +0900

    kinakomochi

--- scripts/bot.js
+++ scripts/bot.js
@@ -1,17 +1,21 @@
 module.exports = robot => {
     // "@botName hoge"を受け取ったら"@senderName fuga"を送り返す
-    robot.respond(/hoge$/i, res => {
+    robot.respond(/.*hoge.*$/i, res => {
         res.reply("huga");
     });
 
-    robot.respond(/やんね！$/i, res => {
+    robot.respond(/.*やんね！.*$/i, res => {
         res.reply("やんね！");
     });
 
-    robot.respond(/もちもち$/i, res => {
+    robot.respond(/.*もちもち.*$/i, res => {
         res.reply("きなこもち～～～～～！");
     });
-    robot.respond(/できること$/i, res => {
+    robot.respond(/.*きなこもち.*$/i, res => {
+        res.reply(":kinako.ex-large:");
+    });
+    robot.respond(/.*できること.*$/i, res => {
         res.reply(":null:");
     });
+
 };
