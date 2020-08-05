ad99a3951b1d32028529824c05830e91f181c42b
Ras <asymptote.k.k@gmail.com>
2020-08-05 17:44:43 +0900

    機能の分化

--- scripts/control.js
+++ scripts/control.js
@@ -1,278 +1,140 @@
-function String_random (pattern) {
-	if (pattern instanceof RegExp) pattern = pattern.source;
+const { User } = require("hubot");
 
-	function processGrouping (pattern) {
-		var tree  = [];
-		var stack = [ tree ];
-		var n = 1;
-		while (pattern.length) {
-			var chr = pattern.shift();
-			if (chr === '\\') {
-				var next = pattern.shift();
-				if (next === '(' || next === ')') {
-					stack[0].push(next);
-				} else {
-					stack[0].push(chr, next);
-				}
-			} else
-			if (chr === '(') {
-				var inner = [];
-				stack[0].push(inner);
-				stack.unshift(inner);
+module.exports = robot => {
 
-				var next = pattern.shift(); // no warnings
-				if (next === '?') {
-					next = pattern.shift();
-					if (next === ':') {
-						// just create a group
-					} else {
-						throw "Invalid group";
-					}
-				} else
-				if (next === '(' || next === ')') {
-					pattern.unshift(next);
-				} else {
-					inner.n = n++;
-					inner.push(next);
-				}
-			} else
-			if (chr === ')') {
-				stack.shift();
-			} else {
-				stack[0].push(chr);
-			}
-		}
+    //起動時メッセージ
+    deplymessage = ":kinano.ex-large.pyon:";
+    robot.send({channelID: "f58c72a4-14f0-423c-9259-dbb4a90ca35f"},deplymessage);
 
-		if (stack.length > 1) throw "missmatch paren";
+    //``@BOT_kinano responds[i]``を受け取ると``@username replys[i]``を返す
+    for(let i = 0;i < responds.length;i++){
+        robot.respond(responds[i], res => {
+            const {message} = res.message;
+            const {user} = message;
+            if(user.bot)
+                return;
+            else 
+                setTimeout(() => {
+                    res.reply(replys[i]);
+                },500);
+                if(i == 0 || i == 1){
+                    setTimeout(() => {
+                        res.send(
+                            {
+                                type: "stamp",
+                                name: "one"
+                            },
+                            {
+                                type: "stamp",
+                                name: "two"
+                            },
+                            '!{"type":"user","raw":"@Ras","id":"0fa5d740-0841-4b88-b7c8-34a68774c784"}'
+                            );
+                    },1500);
+                }
+        });
+    }
 
-		return tree;
-	}
+    //``@BOT_kinano hears[i]``(監視対象チャンネルではメンション不要)を受け取ると``sends[i]``を返す
+    for(let i = 0;i < hears.length;i++){
+        robot.hear(hears[i], res => {
+            const {message} = res.message;
+            const {user} = message;
+            if(user.bot)
+                return;
+            else if(i === 0) 
+                setTimeout(() => {
+                    res.send(sends[0]);
+                },1000);
+            else 
+                setTimeout(() => {
+                    res.send(sends[i]);
+                },500);
+        });
+    }
 
-	function processSelect (tree) {
-		var candinates = [ [] ];
+    //``@BOT_kinano もふもふ``(監視対象チャンネルではメンション不要)を受け取るとランダム文字列を返す
+    //正規表現使って簡潔に書きたい
+    robot.hear(/.*もふもふ.*/, res => {
+        const {message} = res.message;
+        const {user} = message;
+        if(user.bot)
+            return;
+        else {
+            let r = "";
+            for(let i = 0; i < 2; i++){
+                const generated = String.fromCodePoint(Math.floor(Math.random() * (end - start)) + start);
+                r += generated;
+            }
+            setTimeout(() => {
+                res.reply(r + r);
+            },500);
+        }
+    });
 
-		while (tree.length) {
-			var chr = tree.shift();
-			if (chr === '\\') {
-				var next = tree.shift();
-				if (next === '|') {
-					candinates[0].push(next);
-				} else {
-					candinates[0].push(chr, next);
-				}
-			} else
-			if (chr === '[') {
-				candinates[0].push(chr);
-				while (tree.length) {
-					chr = tree.shift();
-					candinates[0].push(chr);
-					if (chr === '\\') {
-						var next = tree.shift(); // no warnings
-						candinates[0].push(next);
-					} else
-					if (chr === ']') {
-						break;
-					}
-				}
-			} else
-			if (chr === '|') {
-				candinates.unshift([]);
-			} else {
-				candinates[0].push(chr);
-			}
-		}
+    //``@BOT_kinano .*なってる``(監視対象チャンネルではメンション不要)(後方一致)を受け取るとnatterusからランダムで返す
+    robot.hear(/.*なってる$/, res => {
+        const {message} = res.message;
+        const {user} = message;
+        if(user.bot)
+            return;
+        else {
+            let i = Math.floor( Math.random() * natterus.length );
+            setTimeout(() => {
+                res.reply(natterus[i]);
+            },500);
+        }
+    });
 
-		for (var i = 0, it; (it = candinates[i]); i++) {
-			tree.push(it);
-			for (var j = 0, len = it.length; j < len; j++) {
-				if (it[j] instanceof Array) {
-					processSelect(it[j]);
-				}
-			}
-		}
+    //``@BOT_kinano STAMPhears[i]``(監視対象チャンネルではメンション不要)を受け取るとSTAMPsends[i]のスタンプを押す
+    for(let i = 0;i < hears.length;i++){
+        robot.hear(STAMPhears[i], res => {
+            res.send(
+                {
+                    type: "stamp",
+                    name: STAMPsends[i]
+                }
+            );
+        });
+    }
 
-		// 入れ子、 奇数段が pattern 偶数段が candinates,
-		return [ tree ];
-	}
+    //``@BOT_kinano .*(寝|おやすみ|oyasumi|osumiya|oyasta|poyasimi).*``(監視対象チャンネルではメンション不要)(部分一致、()内の言葉ならどれでもよい)を受け取るとsleepsからランダムでスタンプを押す
+    robot.hear(/.*(寝|おやすみ|oyasumi|osumiya|oyasta|poyasimi).*/, res => {
+        let i = Math.floor( Math.random() * sleeps.length );
+        res.send(
+            {
+                type: "stamp",
+                name: sleeps[i]
+            }
+        );
+    });
 
-	var UPPERS = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-	var LOWERS = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-	var DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-	var HIRAS  = ["ぁ", "あ", "ぃ", "い", "ぅ", "う", "ぇ", "え", "ぉ", "お", "か", "が", "き", "ぎ", "く", "ぐ", "け", "げ", "こ", "ご", "さ", "ざ", "し", "じ", "す", "ず", "せ", "ぜ", "そ", "ぞ", "た", "だ", "ち", "ぢ", "っ", "つ", "づ", "て", "で", "と", "ど", "な", "に", "ぬ", "ね", "の", "は", "ば", "ぱ", "ひ", "び", "ぴ", "ふ", "ぶ", "ぷ", "へ", "べ", "ぺ", "ほ", "ぼ", "ぽ", "ま", "み", "む", "め", "も", "ゃ", "や", "ゅ", "ゆ", "ょ", "よ", "ら", "り", "る", "れ", "ろ", "ゎ", "わ", "ゐ", "ゑ", "を", "ん"];
-	var SPACES = [" ", "\n", "\t"];
-	var OTHERS = ["!", "\"", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "`", "{", "|", "}", "~"];
-	var ALL    = [].concat(UPPERS, LOWERS, DIGITS, HIRAS,  " ", OTHERS, ["_"]);
+        //監視対象チャンネルで"おいすー"を受け取ったら"お""い""す""ー"スタンプをランダム順で返す
+    robot.hear(/.*(おいす|:oisu-1::oisu-2::oisu-3::oisu-4yoko:).*/i, res => {
+        res.send(
+            {
+                type: "stamp",
+                name: "oisu-1"
+            }
+        );
+        res.send(
+            {
+                type: "stamp",
+                name: "oisu-2"
+            }
+        );
+        res.send(
+            {
+                type: "stamp",
+                name: "oisu-3"
+            }
+        );
+        res.send(
+            {
+                type: "stamp",
+                name: "oisu-4yoko"
+            }
+        );
+    });
 
-	var CLASSES = {
-		'd' : DIGITS,
-		'D' : [].concat(UPPERS, LOWERS, SPACES, OTHERS, ['_']),
-		'w' : [].concat(UPPERS, LOWERS, DIGITS, HIRAS,  ['_']),
-		'W' : [].concat(SPACES, OTHERS),
-		't' : [ '\t' ],
-		'n' : [ '\n' ],
-		'v' : [ '\u000B' ],
-		'f' : [ '\u000C' ],
-		'r' : [ '\r' ],
-		's' : SPACES,
-		'S' : [].concat(UPPERS, LOWERS, DIGITS, HIRAS,  OTHERS, ['_']),
-		'0' : [ '\0' ]
-	};
-
-	var REFERENCE = {};
-
-	function processOthers (tree) {
-		var ret = '', candinates = [];
-		tree = tree.slice(0);
-
-		function choice () {
-			var ret = candinates[ Math.floor(candinates.length * Math.random()) ];
-			if (ret instanceof Array) {
-				ret = processOthers(ret);
-			}
-			if (candinates.n) REFERENCE[candinates.n] = ret;
-			return ret || '';
-		}
-
-		while (tree.length) {
-			var chr = tree.shift();
-			switch (chr) {
-				case '^':
-				case '$':
-					// do nothing
-					break;
-				case '*':
-					for (var i = 0, len = Math.random() * 10; i < len; i++) {
-						ret += choice();
-					}
-					candinates = [];
-					break;
-				case '+':
-					for (var i = 0, len = Math.random() * 10 + 1; i < len; i++) {
-						ret += choice();
-					}
-					candinates = [];
-					break;
-				case '{':
-					var brace = '';
-					while (tree.length) {
-						chr = tree.shift();
-						if (chr === '}') {
-							break;
-						} else {
-							brace += chr;
-						}
-					}
-
-					if (chr !== '}') throw "missmatch brace: " + chr;
-
-					var dd = brace.split(/,/);
-					var min = +dd[0];
-					var max = (dd.length === 1) ? min : (+dd[1] || 10);
-					for (var i = 0, len = Math.floor(Math.random() * (max - min + 1)) + min; i < len; i++) {
-						ret += choice();
-					}
-					candinates = [];
-					break;
-				case '?':
-					if (Math.random() < 0.5) {
-						ret += choice();
-					}
-					candinates = [];
-
-					break;
-
-				case '\\':
-					ret += choice();
-					var escaped = tree.shift();
-
-					if (escaped.match(/^[1-9]$/)) {
-						candinates = [ REFERENCE[escaped] || '' ];
-					} else {
-						if (escaped === 'b' || escaped === 'B') {
-							throw "\\b and \\B is not supported";
-						}
-						candinates = CLASSES[escaped];
-					}
-
-					if (!candinates) candinates = [ escaped ];
-
-					break;
-				case '[':
-					ret += choice();
-
-					var sets = [], negative = false;
-					while (tree.length) {
-						chr = tree.shift();
-						if (chr === '\\') {
-							var next = tree.shift();
-							if (CLASSES[next]) {
-								sets = sets.concat(CLASSES[next]);
-							} else {
-								sets.push(next);
-							}
-						} else
-						if (chr === ']') {
-							break;
-						} else
-						if (chr === '^') {
-							var before = sets[ sets.length - 1];
-							if (!before) {
-								negative = true;
-							} else {
-								sets.push(chr);
-							}
-						} else
-						if (chr === '-') {
-							var next = tree.shift(); // no warnings
-							if (next === ']') {
-								sets.push(chr);
-								chr = next;
-								break;
-							}
-							var before = sets[ sets.length - 1]; // no warnings
-							if (!before) {
-								sets.push(chr);
-							} else {
-								for (var i = before.charCodeAt(0) + 1, len = next.charCodeAt(0); i < len; i++) {
-									sets.push(String.fromCharCode(i));
-								}
-							}
-						} else {
-							sets.push(chr);
-						}
-					}
-					if (chr !== ']') throw "missmatch bracket: " + chr;
-
-					if (negative) {
-						var neg = {};
-						for (var i = 0, len = sets.length; i < len; i++) {
-							neg[sets[i]] = true;
-						}
-
-						candinates = [];
-						for (var i = 0, len = ALL.length; i < len; i++) {
-							if (!neg[ALL[i]]) candinates.push(ALL[i]);
-						}
-					} else {
-						candinates = sets;
-					}
-					break;
-				case '.':
-					ret += choice();
-					candinates = ALL;
-					break;
-				default:
-					ret += choice();
-					candinates = chr;
-			}
-		}
-		return ret + choice();
-	}
-
-	var tree;
-	tree = processGrouping(pattern.split(''));
-	tree = processSelect(tree);
-	return processOthers(tree);
-}
-
-this.String_random = String_random;
+};
