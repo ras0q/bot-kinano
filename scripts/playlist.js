999e3c92c71918c43c12cbb52649dbbfc9217258
Ras <asymptote.k.k@gmail.com>
2020-08-13 18:14:41 +0900

    playlist復活

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -1,52 +1,52 @@
-// const { User } = require("hubot");
+const { User } = require("hubot");
 
-// let playlist =[];
+let playlist =[];
 
-// module.exports = robot => {
-//     res.respond(/.*%add /i){
-//         const userName = res.message.message.user.name;
-//         const plainText = res.message.message.plainText;
-//         if(playlist == "error"){
-//             setTimeout(() => {
-//                 res.send("えらー")
-//             }, 500);
-//         }
-//         else{
-//             const persentIndex = plainText.indexOf("%");
-//             let musicname = plainText.slice(persentIndex + 4, plainText.end());
-//             let headline = "| 追加した人 | 曲名 |"
-//             let addtolist = "|" + userName + "|" + musicname + "|\n";
-//             playlist.push(addtolist);
-//             setTimeout(() => {
-//                 res.send("ぷれいりすとに追加したやんね！\n"+ headline + "\n"+ addtolist)
-//             }, 500);
-//         }
-//     }
+module.exports = robot => {
+    res.respond(/.*%add /i){
+        const userName = res.message.message.user.name;
+        const plainText = res.message.message.plainText;
+        if(playlist == "error"){
+            setTimeout(() => {
+                res.send("えらー")
+            }, 500);
+        }
+        else{
+            const persentIndex = plainText.indexOf("%");
+            let musicname = plainText.slice(persentIndex + 4, plainText.end());
+            let headline = "| 追加した人 | 曲名 |"
+            let addtolist = "|" + userName + "|" + musicname + "|\n";
+            playlist.push(addtolist);
+            setTimeout(() => {
+                res.send("ぷれいりすとに追加したやんね！\n"+ headline + "\n"+ addtolist)
+            }, 500);
+        }
+    }
 
-//     res.respond(/.*%delete /i){
-//         const plainText = res.message.message.plainText;
-//         if(playlist == "error"){
-//             setTimeout(() => {
-//                 res.send("えらー")
-//             }, 500);
-//         }
-//         else{
-//             const persentIndex = plainText.indexOf("%");
-//             const deletefromlist =plainText.slice(persentIndex + 7, plainText.end());
-//             setTimeout(() => {
-//                 res.send("ぷれいりすとから" + playlist[deletefromlist] + "を削除したやんね！")
-//                 }, 500);
-//             playlist = playlist.splice(deletefromlist,1);
-//         }
-//     }
+    res.respond(/.*%delete /i){
+        const plainText = res.message.message.plainText;
+        if(playlist == "error"){
+            setTimeout(() => {
+                res.send("えらー")
+            }, 500);
+        }
+        else{
+            const persentIndex = plainText.indexOf("%");
+            const deletefromlist =plainText.slice(persentIndex + 7, plainText.end());
+            setTimeout(() => {
+                res.send("ぷれいりすとから" + playlist[deletefromlist] + "を削除したやんね！")
+                }, 500);
+            playlist = playlist.splice(deletefromlist,1);
+        }
+    }
 
-//     res.respond(/.*%watch/i){
-//         let table = "| 追加した人 | 曲名 |\n";
-//         for(let i = 0;i < playlist.length; i++){
-//             table = table + "|" + i + "|" + playlist[i];
-//         }
-//         setTimeout(() => {
-//             res.send("ぷれいりすとやんね～\n" + table)
-//         }, 500);
-//     }
-// }
+    res.respond(/.*%watch/i){
+        let table = "| 追加した人 | 曲名 |\n";
+        for(let i = 0;i < playlist.length; i++){
+            table = table + "|" + i + "|" + playlist[i];
+        }
+        setTimeout(() => {
+            res.send("ぷれいりすとやんね～\n" + table)
+        }, 500);
+    }
+}
