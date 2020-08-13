c469a5bd5abcced38e13d8cf0580649e4b8fcaac
Ras <asymptote.k.k@gmail.com>
2020-08-13 22:56:12 +0900

    fixed

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -3,12 +3,14 @@ let playlist = [
     "|Rozelin|Starry Line|",
     "|Hmcmch| 青春サイダー|",
     "|SSlime|p.h./SEVENTHLINKS - 水槽 (Cover)|",
+    "|Rozelin| Roselia|"
 ]
 
 let URL = [
     "|Rozelin| [Starry Line](https://open.spotify.com/album/2Rvt10dUcULTfzleIhdVIW?si=gYdLfVdeRE-kkJipKP2dJw)|",
     "|Hmcmch| [青春サイダー](https://www.youtube.com/watch?v=ULhJSLevmZ8&feature=youtu.be)|",
     "|SSlime|https://www.youtube.com/watch?v=saFQcNo-rDw&t=30s|",
+    "|Rozelin| [Roselia](https://open.spotify.com/artist/1bljHdc9g7V7ZLkYuf4yfy?si=vKbOBg7VR2KcUgiJtdenlw)|"
 ]
 
 exports.playlist = playlist;
