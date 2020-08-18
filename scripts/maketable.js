e50d6591555a32325339099d2c7edb3dbe690c00
Ras <asymptote.k.k@gmail.com>
2020-08-18 15:13:38 +0900

    fixed

--- scripts/maketable.js
+++ scripts/maketable.js
@@ -1,7 +1,5 @@
-let playlist = require("./playlist.json");
 const playlistURL = require("./words").playlistURL;
-const fs = require('fs');
-
+const fs = require('fs'); //ファイルの読み込み、書き込み
 
 module.exports = robot => {
 
@@ -14,7 +12,7 @@ module.exports = robot => {
         const newlineIndex = plainText.indexOf("\n")
         if(verticalIndex != -1 || newlineIndex != -1){
             setTimeout(() => {
-                res.send("えらー")
+                res.send("こーぶんえらー")
             }, 500);
         }
         else{
@@ -22,19 +20,27 @@ module.exports = robot => {
             const addtable = "|追加した人|追加した曲|\n|-|-|\n" + "|" + userName + "|" + musicName + "|\n"
             setTimeout(() => {
                 res.send("ぷれいりすとに追加したやんね！\n"+ addtable)
-                robot.send({channelID: "37612932-7437-4d99-ba61-f8c69cb85c41"},"プレイリスト追加\n"+ addtable)
             }, 500);
 
             //jsonに追記
-            fs.readFile('playlist.json', 'utf8', function readFileCallback(err, data){
+            fs.readFile('./playlist.json', 'utf8', (err, data) => {
                 if (err){
-                    console.log(err);
-                } else {
+                    res.send("よみこみえらー");
+                }
+                else {
                     obj = JSON.parse(data);
                     obj.list.Push({user: userName, music:musicName});
-                    json = JSON.stringify(obj);
-                    fs.writeFile('playlist.json', json, 'utf8', callback);
-            }});
+                    json = JSON.stringify(obj, null, 4);
+                    fs.writeFile('./playlist.json', json, 'utf8', (err) => {
+                        if (err) {
+                            res.send("かきこみえらー")
+                        }
+                        else {
+                            robot.send({channelID: "37612932-7437-4d99-ba61-f8c69cb85c41"},"プレイリスト追加\n"+ addtable)
+                        }
+                    });
+                }
+            });
         };
     })
 
@@ -42,27 +48,34 @@ module.exports = robot => {
     robot.respond(/.*%delete.*/i, res => {
         const userName = res.message.message.user.name;
         const plainText = res.message.message.plainText;
-        const deleteIndex = plainText.slice(plainText.search(/[0-9]?/));
+        const deleteIndex = plainText.slice(plainText.search(/[0-9]?[0-9]/));
         let deletedUser;
         let deletedMusic;
 
         //jsonから削除
-        fs.readFile('playlist.json', 'utf8', function readFileCallback(err, data){
+        fs.readFile('./playlist.json', 'utf8', (err, data) => {
             if (err){
-                console.log(err);
-            } else {
+                res.send("よみこみえらー");
+            }
+            else {
                 obj = JSON.parse(data);
                 deletedUser = obj.list[deleteIndex].user;
                 deletedMusic = obj.list[deleteIndex].music;
                 delete obj.playlist[deleteIndex];
-                json = JSON.stringify(obj);
-                fs.writeFile('playlist.json', json, 'utf8', callback);
+                json = JSON.stringify(obj, null, 4);
+                fs.writeFile('./playlist.json', json, 'utf8', (err) => {
+            if (err) {
+                res.send("かきこみえらー")
+            }
+            else {
+                robot.send({channelID: "37612932-7437-4d99-ba61-f8c69cb85c41"},"プレイリスト削除\n" + deleteTable)
+            }
+        });
         }});
 
         let deleteTable = "|削除した人|追加した人|削除した曲|\n|-|-|-|\n|" + userName + "|" + deletedUser + "|" + deletedMusic + "|\n";
         setTimeout(() => {
             res.send("ぷれいりすとから 曲" + deleteIndex +" を削除したやんね！\n" + deleteTable)
-            robot.send({channelID: "37612932-7437-4d99-ba61-f8c69cb85c41"},"プレイリスト削除\n" + deleteTable)
         }, 500);
 
     })
@@ -71,9 +84,9 @@ module.exports = robot => {
     robot.respond(/.*%watch$/i, res => {
         let table = "|番号|追加した人|曲名|\n|-|-|-|\n|例|BOT_kinano|きなこもちもちのうた|\n";
 
-        fs.readFile('myjsonfile.json', 'utf8', function readFileCallback(err, data){
+        fs.readFile('./playlist.json', 'utf8', (err, data) => {
             if (err){
-                console.log(err);
+                res.send("よみこみえらー");
             } else {
             obj = JSON.parse(data);
             for(let i = 0;i < playlist.length; i++){
