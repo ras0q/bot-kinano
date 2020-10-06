bc267c5cffcb827e1194f8993c1f6ba31c643cc2
Kira <asymptote.k.k@gmail.com>
2020-10-06 12:39:27 +0900

    wordsのlist化

--- scripts/control.js
+++ scripts/control.js
@@ -1,13 +1,9 @@
-const readme = require("./words").readme;
-const responds = require("./words").responds;
-const replys = require("./words").replys;
-const hears = require("./words").hears;
-const sends = require("./words").sends;
-const start = require("./words").start;
-const end = require("./words").end;
-const natterus = require("./words").natterus;
-const STAMPhears = require("./words").STAMPhears;
-const STAMPsends = require("./words").STAMPsends;
+const {
+    getRandom,
+    getMofu,
+    is_mentioned,
+    is_not_mentioned,
+} = require("./words");
 
 module.exports = robot => {
 
@@ -16,91 +12,47 @@ module.exports = robot => {
     const RasuserID = "0fa5d740-0841-4b88-b7c8-34a68774c784"; //RasのuserID
 
     //起動時メッセージ
-    let r = "";
-    for(let i = 0; i < 2; i++){
-        const generated = String.fromCodePoint(Math.floor(Math.random() * (end - start)) + start);
-        r += generated;
-    }
     robot.send(
         {channelID: RaschannelID},
-        "デプロイ完了やんね～ " + r + r
+        "デプロイ完了やんね～ " + getMofu()
     );
 
-    //help
-    robot.respond(/(できること|help)$/i, res => {
-        const botflag = res.message.message.user.bot;
-        if(!botflag)
-            setTimeout(() => {
-                res.send(readme);
-            },500);
-    });
-
-    //``@BOT_kinano responds[i]``を受け取ると``@username replys[i]``を返す
-    for(let i = 0; i < responds.length; i++){
-        robot.respond(responds[i], res => {
-            const botflag = res.message.message.user.bot;
-            if(!botflag)
+    //メンション付きメッセージ
+    for(let i = 0; i < is_mentioned.length; i++){
+        robot.respond(is_mentioned[i][msg], res => {
+            const { bot } = res.message.message.user;
+            if(!bot){
                 setTimeout(() => {
-                    res.reply(replys[i]);
+                    res.reply(is_mentioned[i][res]);
                 },500);
-        });
-    }
-
-    //``@BOT_kinano hears[i]``(監視対象チャンネルではメンション不要)を受け取ると``sends[i]``を返す
-    for(let i = 0; i < hears.length; i++){
-        robot.hear(hears[i], res => {
-            const botflag = res.message.message.user.bot;
-            if(!botflag){
-                if(i == 0)
-                    setTimeout(() => {
-                        res.send(sends[i]);
-                    },1000);
-                else
-                    setTimeout(() => {
-                        res.send(sends[i]);
-                    },500);
             }
         });
     }
 
-    //``@BOT_kinano もふもふ``(監視対象チャンネルではメンション不要)を受け取るとランダム文字列を返す
-    robot.hear(/^.*もふもふ.*/, res => {
-        const botflag = res.message.message.user.bot;
-        if(!botflag){
-            let r = "";
-            for(let i = 0; i < 2; i++){
-                const generated = String.fromCodePoint(Math.floor(Math.random() * (end - start)) + start);
-                r += generated;
+    //メンション無しメッセージ
+    for(let i = 0; i < is_not_mentioned.length; i++){
+        robot.hear(is_not_mentioned[i][msg], res => {
+            const { bot } = res.message.message.user;
+            if(!bot){
+                setTimeout(() => {
+                    res.send(is_not_mentioned[i][res]);
+                },500);
             }
-            setTimeout(() => {
-                res.reply(r + r);
-            },500);
-        }
-    });
-
-    //``@BOT_kinano .*なってる``(監視対象チャンネルではメンション不要)(後方一致)を受け取るとnatterusからランダムで返す
-    robot.hear(/^.*なってる$/, res => {
-        const botflag = res.message.message.user.bot;
-        if(!botflag){
-            let i = Math.floor( Math.random() * natterus.length );
-            setTimeout(() => {
-                res.reply(natterus[i]);
-            },500);
-        }
-    });
+        });
+    }
 
     //``ABCやるか``を受け取るとランダムで問題を返す
-    robot.hear(/^.*ABCや.*/, res => {
-        const botflag = res.message.message.user.bot;
-        const plainText = res.message.message.plainText;
-        if(!botflag){
+    robot.hear(/^.*ABCや.*/i, res => {
+        const { message } = res.message;
+        const { bot, plainText } = message;
+        if(!bot){
             let slashindex = plainText.indexOf("/");
-            let recent = 178; //最新回
+            let recent = 179; //最新回
             let min = 126;
-            let index = ("00" + (Math.floor(Math.random() * (recent + 1 - min)) + min)).slice(-3);
+            let index = ("00" + (Math.floor(getRandom(min, recent+1)).slice(-3)));
             let diff = "";
             if(slashindex != -1 && plainText[slashindex + 1].match(/[a-f]/i)) diff = plainText[slashindex + 1];
-            else diff = String.fromCodePoint(Math.floor(Math.random() * 6) + 97);
+            else diff = String.fromCodePoint(getRandom("a".codePointAt(0), "f".codePointAt(0)));
             setTimeout(() => {
                 res.reply("今日の問題はこれやんね！\nhttps://atcoder.jp/contests/abc" + index + "/tasks/abc" + index + "_" + diff)
             })
@@ -109,9 +61,9 @@ module.exports = robot => {
 
     const oisuregexp = /(お|o|O|オ|:oisu-1(.large|.ex-large|.small|.rotate|.wiggle|.parrot|.zoom|.inversion|.turn|.turn-v|.happa|.pyon|.flashy|.pull|.atsumori|.stretch|.stretch-v|.conga|.rainbow|.ascension|.shake|.party|.attract){0,5}:){3}/g;
     robot.hear(/^.*(お|o|O|オ|:oisu-1(.large|.ex-large|.small|.rotate|.wiggle|.parrot|.zoom|.inversion|.turn|.turn-v|.happa|.pyon|.flashy|.pull|.atsumori|.stretch|.stretch-v|.conga|.rainbow|.ascension|.shake|.party|.attract){0,5}:){3}.*/, res => {
-        const botflag = res.message.message.user.bot;
+        const { bot } = res.message.message.user.bot;
         let plainText = res.message.message.plainText;
-        if(!botflag){
+        if(!bot){
             let r = "";
             while(plainText.search(oisuregexp) != -1){
                 plainText = plainText.substr(plainText.search(oisuregexp)+3)
@@ -122,70 +74,15 @@ module.exports = robot => {
         }
     })
 
-    //``@BOT_kinano STAMPhears[i]``(監視対象チャンネルではメンション不要)を受け取るとSTAMPsends[i]のスタンプを押す
-    for(let i = 0; i < STAMPhears.length; i++){
-        robot.hear(STAMPhears[i], res => {
-            const botflag = res.message.message.user.bot;
-            if(!botflag){
-                res.send(
-                    {
-                        type: "stamp",
-                        name: STAMPsends[i]
-                    }
-                );
-            }
-        });
-    }
-
-    //``@BOT_kinano .*(寝|おやすみ|oyasumi|osumiya|oyasta|poyasimi).*``(監視対象チャンネルではメンション不要)を受け取るとランダム返信
-    robot.hear(/^.*(寝|おやすみ|oyasumi|osumiya|oyasta|poyasimi).*/, res => {
-        const botflag = res.message.message.user.bot;
-        if(!botflag){
-            let i = Math.random();
-            setTimeout(() => {
-                if(i > 0.9) res.send("えへへ")
-                else if(i > 0.8) res.send("寝るな！")
-                else res.send(
-                    {
-                        type: "stamp",
-                        name: "oyasumi"
-                    }
-                );
-            }, 500);
-        }
-    });
-
-        //監視対象チャンネルで"おいすー"を受け取ったら"お""い""す""ー"スタンプをランダム順で返す
-    robot.hear(/^.*([おぉオォｵｫ]|(:oisu\-1.*:))([いぃイィｲｨｨ]|(:oisu\-2.*:))([すスｽ]|(:oisu\-3.*:)).*/i, res => {
-        for(let i = 1; i < 5; i++){
-            if(i == 4){
-                res.send(
-                    {
-                        type: "stamp",
-                        name: "oisu-4yoko"
-                    }
-                );
-            }
-            else{
-                res.send(
-                    {
-                        type: "stamp",
-                        name: "oisu-" + i
-                    }
-                );
-            }
-        }
-    });
-
     //メッセージの時間を返す
     robot.hear(/^\/.*/, res => {
-        const MessageID = res.message.message.id;
-        const time = res.message.message.createdAt;
-        const time2 = time.slice(0,-1);
+        const { message } = res.message;
+        const { id, createdAt } = message;
+        const time2 = createdAt.slice(0,-1);
         const JPNhour = (Number(time2.substr(11,2)) + 9) % 24; //日本時間に変換
         const JPNhourStr = ("0" + JPNhour).slice(-2);
         const JPNtime = time2.replace(/T../, " " + JPNhourStr);
-        res.send(JPNtime + "\n https://q.trap.jp/messages/" + MessageID );
+        res.send(JPNtime + "\n https://q.trap.jp/messages/" + id );
     })
 
 };
--- scripts/words.js
+++ scripts/words.js
@@ -1,62 +1,132 @@
-//コマンド一覧
-
-//helpメッセージ
 const fs = require('fs');
 const readme = fs.readFileSync("./README.md", 'utf8');
 
-//``@BOT_kinano responds[i]``を受け取ると``@username replys[i]``を返す
-const responds = [
-    /.*hoge.*/i,
-    /.*もちもち.*/,
-];
-const replys = [
-    "huga",
-    "きなこもち～～～～～！",
-];
+//start以上end未満の乱数を返す
+const getRandom　= (start, end) => {
+    return Math.floor(Math.random() * (end - start)) + start;
+}
 
+const getMofu = () => {
+    let r = "";
+    for(let i = 0; i < 2; i++){
+        const generated = String.fromCodePoint(getMofu("ぁ".codePointAt(0), "ん".codePointAt(0)+1));
+        r += generated;
+    }
+    return r + r;
+}
 
-//``@BOT_kinano hears[i]``(監視対象チャンネルではメンション不要)を受け取ると``sends[i]``を返す
-const hears = [
-    /^.*Ras.*/i,
-    /^.*もちもち.*/,
-    /^.*(きなこ|kinako).*/,
-    /^.*(きなの|kinano).*/,
-    /^.*やんね.*/,
-    /^.*([おぉオォｵｫ]|(:oisu\-1.*:))([いぃイィｲｨｨ]|(:oisu\-2.*:))(?!([すスｽ]|(:oisu\-3.*:))).*/,
-    /^.*(か[あ～]|car|[っう]かな|[やす]るぞ).*/, //後方一致どうしよう
-    /^.*うまうま.*/,
-    /^.*[い言云]ってい?る.*/,
-    /^.*(死|:si.*:).*/,
-    /^.*(おなか|お腹).*/,
-    /^.*(もふもち|もちもふ).*/,
-    /^.*(助けて|たすけて|tasukete).*/
-];
-const sends = [
-    "えへへ",
-    "もちもち～:blobenjoy:",
-    ":kinako.ex-large:",
-    "えへえへ",
-    "やんね！",
-    "おい！",
-    "いいぞいいぞ",
-    "むしゃむしゃ",
-    "いうな！",
-    "死ぬな！",
-    "ぽんぽん！",
-    "言いすぎやんね！！！:gao-.ex-large::anger.small.wiggle.wiggle:",
-    readme
+const is_mentioned = [
+    {
+        msg: /.*hoge.*/i,
+        res: "huga"
+    },
+    {
+        msg: /(できること|help)$/i,
+        res: readme,
+    },
+    {
+        msg: /.*もちもち.*/,
+        res: "きなこもち～～～～～！"
+    }
 ];
 
+const is_not_mentioned = [
+    {
+        msg: /^.*Ras.*/i,
+        res:{
+                type: "stamp",
+                name: "Rascal"
+            }
+    },
+    {
+        msg: /^.*もちもち.*/,
+        res: "もちもち～:blobenjoy:"
+    },
+    {
+        msg: /^.*(きなこ|kinako).*/,
+        res: ":kinako.ex-large:"
+    },
+    {
+        msg: /^.*(きなの|kinano).*/,
+        res: "えへえへ"
+    },
+    {
+        msg: /^.*やんね.*/,
+        res: "やんね！"
+    },
+    {
+        msg: /^.*([おぉオォｵｫ]|(:oisu\-1.*:))([いぃイィｲｨｨ]|(:oisu\-2.*:))(?!([すスｽ]|(:oisu\-3.*:))).*/,
+        res: "おい！"
+    },
+    {
+        msg: /^.*(か[あ～]|car|[っう]かな|[やす]るぞ).*/,
+        res: "いいぞいいぞ"
+    },
+    {
+        msg: /^.*うまうま.*/,
+        res: "むしゃむしゃ"
+    },
+    {
+        msg: /^.*[い言云]ってい?る.*/,
+        res: "いうな！"
+    },
+    {
+        msg: /^.*(死|:si.*:).*/,
+        res: "死ぬな！"
+    },
+    {
+        msg: /^.*(おなか|お腹).*/,
+        res: "ぽんぽん！"
+    },
+    {
+        msg: /^.*(助けて|たすけて|tasukete).*]/,
+        res: readme
+    },
+    {
+        msg: /^.*もふもふ.*/,
+        res: getMofu("ぁ".codePointAt(0), "ん".codePointAt(0)+1)
+    },
+    {
+        msg: /^.*なってる$/,
+        res: natterus[getRandom(0, natterus.length)]
+    },
+    {
+        msg: /^.*(寝|おやすみ|oyasumi|osumiya|oyasta|poyasimi).*/,
+        res: sleeps[getRandom(0, sleeps.length)]
+    },
+    {
+        msg: /^.*([おぉオォｵｫ]|(:oisu\-1.*:))([いぃイィｲｨｨ]|(:oisu\-2.*:))([すスｽ]|(:oisu\-3.*:)).*/i,
+        res: {
+            type: "stamp",
+            name: "oisu-1"
+        }
+    },
+    {
+        msg: /^.*([おぉオォｵｫ]|(:oisu\-1.*:))([いぃイィｲｨｨ]|(:oisu\-2.*:))([すスｽ]|(:oisu\-3.*:)).*/i,
+        res: {
+            type: "stamp",
+            name: "oisu-2"
+        }
+    },
+    {
+        msg: /^.*([おぉオォｵｫ]|(:oisu\-1.*:))([いぃイィｲｨｨ]|(:oisu\-2.*:))([すスｽ]|(:oisu\-3.*:)).*/i,
+        res: {
+            type: "stamp",
+            name: "oisu-3"
+        }
+    },
+    {
+        msg: /^.*([おぉオォｵｫ]|(:oisu\-1.*:))([いぃイィｲｨｨ]|(:oisu\-2.*:))([すスｽ]|(:oisu\-3.*:)).*/i,
+        res: {
+            type: "stamp",
+            name: "oisu-4yoko"
+        }
+    }
+];
 
-//unicodeからひらがなを取得(``@BOT_kinano もふもふ``を受け取った時にランダム文字列を返すのに使用)
-const start = "ぁ".codePointAt(0);
-const end = "ん".codePointAt(0);
-
-
-//``@BOT_kinano .*なってる``(監視対象チャンネルではメンション不要)(後方一致)を受け取るとnatterusからランダムで返す
 const natterus = [
     ":yaya::koreni_natteru.large:",
-    ":koreni_natteru.ex-large:",
+    ":koreni_natteru.ex-large.rotate:",
     "なるな！",
     "なるな！",
     "なるな！",
@@ -65,25 +135,46 @@ const natterus = [
     "なるな！",
 ]
 
+const sleeps = [
+    "おやすみやんね～えへへ",
+    "寝るな！",
+    {
+        type: "stamp",
+        name: "oyasumi"
+    },
+    {
+        type: "stamp",
+        name: "oyasumi"
+    },
+    {
+        type: "stamp",
+        name: "oyasumi"
+    },
+    {
+        type: "stamp",
+        name: "oyasumi"
+    },
+    {
+        type: "stamp",
+        name: "oyasumi"
+    },
+    {
+        type: "stamp",
+        name: "oyasumi"
+    },
+    {
+        type: "stamp",
+        name: "oyasumi"
+    },
+    {
+        type: "stamp",
+        name: "oyasumi"
+    }
+]
 
-//``@BOT_kinano STAMPhears[i]``(監視対象チャンネルではメンション不要)を受け取るとSTAMPsends[i]のスタンプを押す
-const STAMPhears = [
-    /^.*Ras.*/i,
-    /^.*(おは|ohagoza|ohasta).*/i,
-];
-const STAMPsends = [
-    "rascal",
-    "ohagoza",
-];
-
-exports.readme = readme;
-exports.responds = responds;
-exports.replys = replys;
-exports.hears = hears;
-exports.sends = sends;
-exports.start = start;
-exports.end = end;
+exports.getRandom = getRandom;
+exports.getMofu = getMofu;
+exports.is_mentioned = is_mentioned;
+exports.is_not_mentioned = is_not_mentioned;
 exports.natterus = natterus;
-exports.STAMPhears = STAMPhears;
-exports.STAMPsends = STAMPsends;
 
