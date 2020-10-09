abc39320c3f1db35b20955b7c425a48a2ed4cdff
Kira <asymptote.k.k@gmail.com>
2020-10-09 19:11:20 +0900

    maketable調整

--- scripts/control.js
+++ scripts/control.js
@@ -25,7 +25,6 @@ module.exports = robot => {
 
     const gtRB_ID = "2a5616d5-5d69-4716-8377-1e1fb33278fe"; //#gps/times/Ras/Bot
 
-
     //起動時メッセージ
     robot.send(
         {channelID: gtRB_ID},
--- scripts/maketable.js
+++ scripts/maketable.js
@@ -3,6 +3,7 @@ const fs = require('fs'); //ファイルの読み込み、書き込み
 
 module.exports = robot => {
 
+    const gtRB_ID = "2a5616d5-5d69-4716-8377-1e1fb33278fe"; //#gps/times/Ras/Bot
     const DM_ID = "37612932-7437-4d99-ba61-f8c69cb85c41"; //Ras-BOT_kinanoのDM
     const RasuserID = "0fa5d740-0841-4b88-b7c8-34a68774c784"; //RasのuserID
 
@@ -48,6 +49,7 @@ module.exports = robot => {
     robot.hear(/^%add.*/i, res => {
         const userName = res.message.message.user.name;
         const plainText = res.message.message.plainText;
+        const channelID = res.message.message.channelId;
         const kagikakko = plainText.indexOf("["); //URLがあれば読み取る
         const kakko = plainText.indexOf("](");
         const kakkotoji = plainText.indexOf(")");
@@ -85,10 +87,10 @@ module.exports = robot => {
                         }
                         else {
                             const addtable = "|追加した人|追加した曲|曲のURL|\n|-|-|-|\n" + "|" + userName + "|" + musicName + "|" + URL + "|\n" //追加曲の表を作成
-                            robot.send({channelID: DM_ID},"プレイリスト追加\n"+ addtable) //RasへのDMに通知
+                            robot.send({channelID: gtRB_ID},"**プレイリスト追加**\n"+ addtable) //RasへのDMに通知
                             robot.send({channelID: DM_ID},json)
                             setTimeout(() => {
-                                res.send("ぷれいりすとに追加したやんね！\n"+ addtable) //追加成功時メッセージ
+                                if(channelID != gtRB_ID) res.send("ぷれいりすとに追加したやんね！\n"+ addtable) //追加成功時メッセージ
                             }, 500); //メッセージ順逆転防止
                         }
                     });
@@ -128,10 +130,10 @@ module.exports = robot => {
                         }
                         else {
                             const removeTable = "|削除した人|追加した人|削除した曲|曲のURL|\n|-|-|-|-|\n|" + userName + "|" + removedUser + "|" + removedMusic + "|" + removedURL + "|\n"; //削除曲の表作成
-                            robot.send({channelID: DM_ID},"プレイリスト削除\n" + removeTable) //RasへのDMに通知
+                            robot.send({channelID: gtRB_ID},"プレイリスト削除\n" + removeTable) //RasへのDMに通知
                             robot.send({channelID: DM_ID},json)
                             setTimeout(() => {
-                                res.send("ぷれいりすとから 曲" + removeIndex +" を削除したやんね！\n" + removeTable) //削除成功時メッセージ
+                                if(channelID != gtRB_ID) res.send("ぷれいりすとから 曲" + removeIndex +" を削除したやんね！\n" + removeTable) //削除成功時メッセージ
                             }, 500); //メッセージ順逆転防止
                         }
                     });
@@ -143,25 +145,28 @@ module.exports = robot => {
 
     //曲確認
     robot.hear(/^%watch$/i, res => {
-        let table = "|番号|追加した人|曲名|\n|-|-|-|\n|例|BOT_kinano|きなこもちもちのうた|\n"; //表の項目と例
-        //playlist.jsonを読み込む
-        fs.readFile('./scripts/playlist.json', 'utf8', (err, data) => {
-            if (err){
-                res.send("よみこみえらー:eyes:"); //読み込み失敗時メッセージ
-            }
-            else {
-                obj = JSON.parse(data); //json文字列をオブジェクトに
-                //表作成
-                for(let i = 0;i < obj.list.length; i++){
-                    const user = obj.list[i].user;
+        const channelID = res.message.message.channelId;
+        if(channelID != DM_ID){
+            let table = "|番号|追加した人|曲名|\n|-|-|-|\n|例|BOT_kinano|きなこもちもちのうた|\n"; //表の項目と例
+            //playlist.jsonを読み込む
+            fs.readFile('./scripts/playlist.json', 'utf8', (err, data) => {
+                if (err){
+                    res.send("よみこみえらー:eyes:"); //読み込み失敗時メッセージ
+                }
+                else {
+                    obj = JSON.parse(data); //json文字列をオブジェクトに
+                    //表作成
+                    for(let i = 0;i < obj.list.length; i++){
+                        const user = obj.list[i].user;
                     const music = obj.list[i].music;
                     table = table + "|" + i + "|" + user + "|" + music + "|\n";
+                    }
+                    setTimeout(() => {
+                        res.send("ぷれいりすとやんね～\n" + table) //表作成成功時メッセージ
+                    }, 500); //メッセージ順逆転防止
                 }
-                setTimeout(() => {
-                    res.send("ぷれいりすとやんね～\n" + table) //表作成成功時メッセージ
-                }, 500); //メッセージ順逆転防止
-            }
-        });
+            });
+        }
     })
 
     //URL確認
