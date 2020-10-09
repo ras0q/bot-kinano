dc5f394b77d4dffd012137ac03a70355ae60d649
Kira <asymptote.k.k@gmail.com>
2020-10-09 12:35:18 +0900

    fixed

--- scripts/post.js
+++ scripts/post.js
@@ -6,7 +6,7 @@ module.exports = robot => {
     //毎朝traPの記事を#gps/times/Rasに投げる
     const RaschannelID = "f58c72a4-14f0-423c-9259-dbb4a90ca35f";
     const recent = 1130; //最新回
-    cron.schedule('0 35 12 * * *', () => {
+    cron.schedule('0 40 12 * * *', () => {
         robot.send({channelID: RaschannelID}, "[](https://trap.jp/post/" + getRandom(0,recent+1) + "/)")
     });
 }
