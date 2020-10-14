b675bf3848b2710c14ad895f91dda1a508b3499b
Kira <asymptote.k.k@gmail.com>
2020-10-15 01:22:32 +0900

    fixed

--- scripts/chat.js
+++ scripts/chat.js
@@ -5,28 +5,30 @@ const APIkey = process.env.CHAPLUS_API_KEY;
 module.exports = robot => {
 
     robot.hear(/^hey kinano/i, res => {
-        const { plainText } = res.message.message;
-        const { displayName } = res.message.message.user;
-        var options = {
-            uri: "https://www.chaplus.jp/v1/chat",
-            qs: {
-                "apikey": APIkey
-            },
-            headers: {
-                "Content-type": "application/json",
-            },
-            json: {
-                "utterance": plainText.slice(6),
-                "username": displayName,
-                "agentState": {
-                    "agentName": "きなの",
-                    "tone": "koshu",
-                    "age": "14"
+        const { message } = res.message;
+        const { plainText, user } = message;
+        const { displayName, bot } = user;
+        if(!bot){
+            var options = {
+                uri: "https://www.chaplus.jp/v1/chat",
+                qs: {
+                    "apikey": APIkey
+                },
+                headers: {
+                    "Content-type": "application/json",
+                },
+                json: {
+                    "utterance": plainText.slice(10),
+                    "username": displayName,
+                    "agentState": {
+                        "agentName": "きなの",
+                        "age": "14"
+                    }
                 }
-            }
-        };
+            };
             request.post(options, function(error, response, body){
                 res.reply(body.bestResponse.utterance)
-        });
+            });
+        }
     })
 }
