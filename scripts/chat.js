4ded3cb12f19721dc358992a5e17ba0e174465d3
Kira <asymptote.k.k@gmail.com>
2020-10-15 11:19:18 +0900

    fixed

--- scripts/chat.js
+++ scripts/chat.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const request = require('request');
 const APIkey = process.env.CHAPLUS_API_KEY;
 
+//start以上end未満の乱数を返す
+const getRandom　= (start, end) => {
+    return Math.floor(Math.random() * (end - start)) + start;
+}
+
+
 module.exports = robot => {
 
     const gtRB_ID = "2a5616d5-5d69-4716-8377-1e1fb33278fe"; //#gps/times/Ras/Bot
@@ -12,6 +18,9 @@ module.exports = robot => {
         const { message } = res.message;
         const { plainText, user } = message;
         const { displayName, bot } = user;
+        // const plainText = "きなのおはよう"
+        // const displayName = "らす"
+        // const bot = false;
         if(!bot){
         let options = {
                 uri: "https://www.chaplus.jp/v1/chat",
@@ -36,7 +45,8 @@ module.exports = robot => {
                         body.status + "\n" + body.message
                     )
                 }
-                res.reply(body.bestResponse.utterance + "(score: " + body.bestResponse.score + ")")
+                const i = getRandom(0,(body.responses).length);
+                res.reply(body.responses[i].utterance + "(score: " + body.responses[i].score + ")")
             });
         }
     })
