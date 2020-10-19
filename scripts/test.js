319cfda941e8d42147d66fd5098de2b8ad6efd7d
Kira <asymptote.k.k@gmail.com>
2020-10-19 16:46:00 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -2,10 +2,10 @@ module.exports = robot => {
     const gtRB_ID = "2a5616d5-5d69-4716-8377-1e1fb33278fe"; //#gps/times/Ras/Bot
 
     robot.catchAll(res => {
-        const { type, stamps } = res.message;
+        const { type, stamps, messageId } = res.message;
         const { stampId } = stamps[0];
-        if(type == "BotMessageStampsUpdated" && stampId == "e43a01b3-48bb-44b6-bca9-733706676641"){
-            robot.send({userID: stamps[0].userId},"2人の世界へようこそ、ここでは2人きりで会話ができるやんね...")
+        if(type == "BotMessageStampsUpdated"){
+            robot.send({channelID: gtRB_ID},`**${stampId}** was pushed or deleted\nhttps://q.trap.jp/messages/${messageId}`)
         }
     })
 }
