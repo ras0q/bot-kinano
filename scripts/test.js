b288019ba96a691710918461e27e38f6b138640d
Kira <asymptote.k.k@gmail.com>
2020-10-19 16:25:26 +0900

    test

---
+++ scripts/test.js
@@ -0,0 +1,10 @@
+module.exports = robot => {
+    const gtRB_ID = "2a5616d5-5d69-4716-8377-1e1fb33278fe"; //#gps/times/Ras/Bot
+
+    robot.catchAll(res => {
+        const { type, stamps } = res.message;
+        if(type == "BotMessageStampsUpdated"){
+            robot.send({userID: stamps[0].userId},stamps.stampsId)
+        }
+    })
+}
