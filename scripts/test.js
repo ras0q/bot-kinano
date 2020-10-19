805059c6ea31c1ba3142804a715068656fef5aab
Kira <asymptote.k.k@gmail.com>
2020-10-19 16:00:55 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,7 +1,9 @@
-// module.exports = robot => {
-//     robot.catchAll(res => {
-//         if(res.message.type = "BotMessageStampsUpdated"){
-//             res.send("押すな！");
-//         }
-//     })
-// }
+module.exports = robot => {
+    const gtRB_ID = "2a5616d5-5d69-4716-8377-1e1fb33278fe"; //#gps/times/Ras/Bot
+
+    robot.catchAll(res => {
+        if(res.message.type == "BotMessageStampsUpdated" && res.message.stamps.userId == "0fa5d740-0841-4b88-b7c8-34a68774c784"){
+            res.send("押すな！");
+        }
+    })
+}
