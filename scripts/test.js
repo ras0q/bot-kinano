62e9c82562775b8423949baecf9b9769b7fc191a
Kira <asymptote.k.k@gmail.com>
2020-10-19 16:08:30 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -2,9 +2,9 @@ module.exports = robot => {
     const gtRB_ID = "2a5616d5-5d69-4716-8377-1e1fb33278fe"; //#gps/times/Ras/Bot
 
     robot.catchAll(res => {
-        // if(res.message.type == "BotMessageStampsUpdated"){
-        //     res.send("押すな！");
-        // }
-        res.send(res.message.type);
+        if(res.message.type == "BotMessageStampsUpdated"){
+            res.send("押すな！");
+        }
+        robot.send({channelID: gtRB_ID}, `${res.message.type} was done`);
     })
 }
