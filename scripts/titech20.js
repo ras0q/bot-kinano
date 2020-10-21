30db70f2d76d92d52bcc488ab47583be46adad75
Kira <asymptote.k.k@gmail.com>
2020-10-21 14:53:17 +0900

    fixed

--- scripts/titech20.js
+++ scripts/titech20.js
@@ -13,15 +13,14 @@ module.exports = robot => {
   const logID = "82b9f8ad-17d9-4597-88f1-0375247a2487";
 
   cron.schedule('0 30 * * * * ', () => {
-  const params = {list_id: "1236878351165321216", count: 200, include_rts: false};
-  client.get('lists/statuses', params, function(error, body, response) {
-    let m = 5;
-    let index = -1
-    for(let i = 0; i < body.length; i++){
-      if(body[i].retweet_count > m) index = i;
-    }
-    if(index != -1) robot.send({channelID: logID}`今話題のツイートやんね！[](https://twitter.com/${ screen_name}/status/${obj[index].in_reply_to_status_id_str})`);
-    else robot.send({channelID: logID}, "今は何も話題になってないやんね、、、")
-  })
+    const params = {list_id: "1236878351165321216", count: 200, include_rts: false};
+    client.get('lists/statuses', params, function(error, body, response) {
+      let m = 5;
+      let index = -1
+      for(let i = 0; i < body.length; i++){
+        if(body[i].retweet_count > m) index = i;
+      }
+      if(index != -1) robot.send({channelID: logID}`[](https://twitter.com/${ screen_name}/status/${obj[index].in_reply_to_status_id_str})`);
+    })
   })
 }
