c928631672ddc6e52d5b7e7fe637ba508e27b70d
Kira <asymptote.k.k@gmail.com>
2020-11-04 13:17:39 +0900

    add post

--- scripts/post.js
+++ scripts/post.js
@@ -9,11 +9,16 @@ module.exports = robot => {
   //毎日8時,13時,17時にtraPの記事を#gps/times/Ras/Bot/gtRに投げる
   const gtRID = "f58c72a4-14f0-423c-9259-dbb4a90ca35f";
   const recent = 1134; //最新回
+  //#gps/times/Ras(8,13,17)
   cron.schedule('0 0 4,8,23 * * *', () => {
     robot.send({channelID: gtRID}, `https://trap.jp/post/${getRandom(0,recent+1)}/`)
   });
-
+  //#gps/times/Z(8)
   cron.schedule('0 0 23 * * *', () => {
     robot.send({channelID: "2937b540-2991-44ce-91dd-504dd29f01e7"}, `https://trap.jp/post/${getRandom(0,recent+1)}/`)
+  });
+  //#gps/times/Kamijo(8)
+  cron.schedule('0 0 12,23 * * *', () => {
+    robot.send({channelID: "2fab81dd-a750-4699-a9c5-3fc13ab9bcee"}, `https://trap.jp/post/${getRandom(0,recent+1)}/`)
   });
 }
