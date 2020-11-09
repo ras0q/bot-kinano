d9e9114fe32d30c69f8005ab8d0ae16a40f2e1e6
Kira <asymptote.k.k@gmail.com>
2020-11-09 22:28:25 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -3,6 +3,9 @@ module.exports = robot => {
         const { type, messageId, stamps } = res.message;
         if(type == "BotMessageStampsUpdated"){
             robot.send({userID: "0fa5d740-0841-4b88-b7c8-34a68774c784"},JSON.stringify(stamps));
+            if(stamps.stampId == "4a7315c0-c8d8-4a65-b6f4-5866a29bd113") {
+                robot.send({channelID: "2a5616d5-5d69-4716-8377-1e1fb33278fe"}, "よくないけどね")
+            }
         }
     })
 }
