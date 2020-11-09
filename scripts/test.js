a362a51c31445acc4471a981ae119ff147133a29
Kira <asymptote.k.k@gmail.com>
2020-11-10 00:01:13 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,8 +1,8 @@
-// module.exports = robot => {
-//     robot.catchAll(res => {
-//         const { type } = res.message;
-//         if(type == "BotMessageStampsUpdated"){
-//             robot.send({channelID: "2a5616d5-5d69-4716-8377-1e1fb33278fe"},JSON.stringify(res.message));
-//         }
-//     })
-// }
+module.exports = robot => {
+    robot.catchAll(res => {
+        const { type } = res.message;
+        if(type == "BotMessageStampsUpdated"){
+            robot.send({channelID: "2a5616d5-5d69-4716-8377-1e1fb33278fe"},JSON.stringify(res.message));
+        }
+    })
+}
