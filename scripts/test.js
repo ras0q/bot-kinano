c815a131092af5cb0f1bb3ad3bf1d785318d92e1
Kira <asymptote.k.k@gmail.com>
2020-11-09 21:58:08 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,12 +1,12 @@
-module.exports = robot => {
-    const gtRB_ID = "2a5616d5-5d69-4716-8377-1e1fb33278fe"; //#gps/times/Ras/Bot
+// module.exports = robot => {
+//     const gtRB_ID = "2a5616d5-5d69-4716-8377-1e1fb33278fe"; //#gps/times/Ras/Bot
 
-    robot.catchAll(res => {
-        const { type, messageId, stamps } = res.message;
-        if(type == "BotMessageStampsUpdated"){
-            robot.send({channelID: gtRB_ID},messageId);
-            robot.send({channelID: gtRB_ID},JSON.stringify(stamps));
-            robot.send({channelID: gtRB_ID},type);
-        }
-    })
-}
+//     robot.catchAll(res => {
+//         const { type, messageId, stamps } = res.message;
+//         if(type == "BotMessageStampsUpdated"){
+//             robot.send({channelID: gtRB_ID},messageId);
+//             robot.send({channelID: gtRB_ID},JSON.stringify(stamps));
+//             robot.send({channelID: gtRB_ID},type);
+//         }
+//     })
+// }
