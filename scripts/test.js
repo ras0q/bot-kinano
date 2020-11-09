1074c605e01fc7acc3cf5be3d502dbc86ac5738b
Kira <asymptote.k.k@gmail.com>
2020-11-09 23:58:09 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -2,7 +2,7 @@ module.exports = robot => {
     robot.catchAll(res => {
         const { type } = res.message;
         if(type == "BotMessageStampsUpdated"){
-            robot.send({channelID: "2a5616d5-5d69-4716-8377-1e1fb33278fe"},JSON.stringify(res));
+            robot.send({channelID: "2a5616d5-5d69-4716-8377-1e1fb33278fe"},JSON.stringify(res.message));
         }
     })
 }
