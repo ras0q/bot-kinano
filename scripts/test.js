222a22a646185b8a4b961111ebf3188350310d13
Kira <asymptote.k.k@gmail.com>
2020-11-16 21:19:23 +0900

    fix test

--- scripts/test.js
+++ scripts/test.js
@@ -4,7 +4,7 @@ const request = require("request");
 module.exports = robot => {
     robot.catchAll(res => {
         const { type, stamps, messageId } = res.message;
-        const { stampName, userId } = stamps;
+        const { stampName, userId } = stamps[0];
         if(type == "BotMessageStampsUpdated" && (stampName == "eenyade" || stampName == "eennyade") && Math.random > 0.8){
             request.get({uri: `https://q.trap.jp/api/v3/users/${userId}`}, (err, res2, body) => {
                 robot.send({userID: "0fa5d740-0841-4b88-b7c8-34a68774c784"},
