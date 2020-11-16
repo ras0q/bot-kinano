e4f6bc5d1cf95645d72ff73429dc7455ad69bca8
Kira <asymptote.k.k@gmail.com>
2020-11-16 22:53:51 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -3,9 +3,7 @@ module.exports = robot => {
     robot.catchAll(res => {
         const { type, stamps, messageId } = res.message;
         const { stampName, userId } = stamps[0];
-        const i = Math.random();
-        console.log(i);
-        if(type == "BotMessageStampsUpdated" && (stampName == "eenyade" || stampName == "eennyade") && i > 0.5){
+        if(type == "BotMessageStampsUpdated" && (stampName == "eenyade" || stampName == "eennyade") && Math.random() > 0.7){
             robot.send({channelID: "f58c72a4-14f0-423c-9259-dbb4a90ca35f"},
                 `!{"type":"user","raw":"いいわけないだろ！！！","id":"${userId}"}\nhttps://q.trap.jp/messages/${messageId}`
             );
