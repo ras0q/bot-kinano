f8890b8d5053f51971984460eb5b054c7400ef14
Kira <asymptote.k.k@gmail.com>
2020-11-16 20:53:09 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -3,7 +3,7 @@ module.exports = robot => {
     robot.catchAll(res => {
         const { type } = res.message;
         if(type == "BotMessageStampsUpdated"){
-            robot.send({userID: "0fa5d740-0841-4b88-b7c8-34a68774c784"},JSON.stringify(res.message));
+            robot.send({userID: "0fa5d740-0841-4b88-b7c8-34a68774c784"},JSON.stringify(res));
         }
     })
 }
