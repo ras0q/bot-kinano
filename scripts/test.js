5ae019576cdba7e0723d16f28d10710d0622f581
Kira <asymptote.k.k@gmail.com>
2020-11-16 21:33:10 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,13 +1,12 @@
 // BotMessageStampsUpdated
 module.exports = robot => {
     robot.catchAll(res => {
-        // const { type, stamps, messageId } = res.message;
-        // const { stampName, userId } = stamps[0];
-        const type = "BotMessageStampsUpdated",stampName = "eenyade", userId = "0fa5d740-0841-4b88-b7c8-34a68774c784", messageId = "hge";
+        const { type, stamps, messageId } = res.message;
+        const { stampName, userId } = stamps[0];
         const i = Math.random();
         console.log(i);
         if(type == "BotMessageStampsUpdated" && (stampName == "eenyade" || stampName == "eennyade") && i > 0.8){
-            robot.send({userID: "0fa5d740-0841-4b88-b7c8-34a68774c784"},
+            robot.send({channelID: "f58c72a4-14f0-423c-9259-dbb4a90ca35f"},
                 `!{"type":"user","raw":"いいわけないだろ！！！","id":"${userId}"}\nhttps://q.trap.jp/messages/${messageId}`
             );
         }
