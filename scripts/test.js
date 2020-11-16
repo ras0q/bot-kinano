1d7cb384891d4fcab9038d52e915e21330b23728
Kira <asymptote.k.k@gmail.com>
2020-11-16 21:31:50 +0900

    test

--- scripts/test.js
+++ scripts/test.js
@@ -1,17 +1,15 @@
-const request = require("request");
-
 // BotMessageStampsUpdated
 module.exports = robot => {
     robot.catchAll(res => {
-        const { type, stamps, messageId } = res.message;
-        const { stampName, userId } = stamps[0];
-        if(type == "BotMessageStampsUpdated" && (stampName == "eenyade" || stampName == "eennyade") && Math.random > 0.8){
-            request.get({uri: `https://q.trap.jp/api/v3/users/${userId}`}, (err, res2, body) => {
-                robot.send({userID: "0fa5d740-0841-4b88-b7c8-34a68774c784"},
-                    `@${body.displayName} いいわけないだろ!\nhttps://q.trap.jp/messages/${messageId}`
-                );
-            })
+        // const { type, stamps, messageId } = res.message;
+        // const { stampName, userId } = stamps[0];
+        const type = "BotMessageStampsUpdated",stampName = "eenyade", userId = "0fa5d740-0841-4b88-b7c8-34a68774c784", messageId = "hge";
+        const i = Math.random();
+        console.log(i);
+        if(type == "BotMessageStampsUpdated" && (stampName == "eenyade" || stampName == "eennyade") && i > 0.8){
+            robot.send({userID: "0fa5d740-0841-4b88-b7c8-34a68774c784"},
+                `!{"type":"user","raw":"いいわけないだろ！！！","id":"${userId}"}\nhttps://q.trap.jp/messages/${messageId}`
+            );
         }
     })
 }
-// Math.random()
