fa2da60503959a4eddd6a48a80ec7e2aa333bf34
Kira <asymptote.k.k@gmail.com>
2020-12-02 18:59:39 +0900

    test chat-test

---
+++ scripts/chat-test.js
@@ -0,0 +1,27 @@
+require('dotenv').config;
+const request = require("request");
+
+const APIkey = process.env.ST_API_KEY;
+
+const option = (message) => {
+    return {
+        uri: "https://api.a3rt.recruit-tech.co.jp/talk/v1/smalltalk",
+        qs: {
+            "apikey": APIkey,
+            "query": message
+        }
+    }
+}
+
+module.exports = robot => {
+    robot.hear(/^test /, res => {
+        const { message } = res.message;
+        const { user, plainText } = message;
+        if(!user.bot){
+            request.post(option(plainText.slice(5)), (err, resp, body) => {
+                if(body.status == 0) res.reply(results[body.reply]);
+                else res.reply("error");
+            })
+        }
+    })
+}
