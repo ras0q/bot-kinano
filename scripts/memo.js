f438f448acba2eae0032b56f5efd20702d230e88
Kira <asymptote.k.k@gmail.com>
2020-12-08 14:26:05 +0900

    fix

--- scripts/memo.js
+++ scripts/memo.js
@@ -48,7 +48,8 @@ module.exports = robot => {
       request.post(option(qs), (error,respond,body) => {
         if(!error){
           if(memo == "") memo = ":404_notfound.ex-large.pyon:";
-          res.send(`## $\\green{\\bf{${name}'s\\, memo\\, was\\, updated!}}$\n${memo}`);
+          const formatedMemo = memo.replace(/\\n/gi, "|\n|");
+          res.send(`|memo|\n|-|\n${formatedMemo}`);
           res.send(
             {
               type: "stamp",
@@ -72,11 +73,12 @@ module.exports = robot => {
       request.get(option(qs), (error,respond,body) => {
         if(!error){
           const { memo } = body;
-          const memo2 = memo + plainText.slice(i + 1);
-          const qs2 = {user: name, memo: memo2};
+          const updatedMemo = memo + plainText.slice(i + 1);
+          const formatedMemo = updatedMemo.replace(/\\n/gi, "|\n|");
+          const qs2 = {user: name, memo: updatedMemo};
           request.post(option(qs2), (error2,respond2,body2) => {
             if(!error2){
-              res.send(`## $\\green{\\bf{${name}'s\\, memo\\, was\\, updated!}}$\n${memo2}`);
+              res.send(`|memo|\n|-|\n${formatedMemo}`);
               res.send(
                 {
                   type: "stamp",
