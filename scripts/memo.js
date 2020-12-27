cb3234afe6b692939696ec9489d45aeecaedd93c
Kira Kawai <asymptote.k.k@gmail.com>
2020-12-27 13:45:25 +0900

    もどした

--- scripts/memo.js
+++ scripts/memo.js
@@ -15,29 +15,29 @@ const option = (Q) => {
 
 module.exports = robot => {
   const gtR_ID ="f58c72a4-14f0-423c-9259-dbb4a90ca35f";
-  // robot.hear(/^(me|め|メ)(mo|も|モ)$/i, res => {
-  //   const { bot, name } = res.message.message.user;
-  //   if(!bot){
-  //     const qs = {user: name};
-  //     request.get(option(qs), (error,respond,body) => {
-  //       if(!error){
-  //         let { user, memo } = body;
-  //         if(memo == "") memo = "\n:404_notfound.ex-large:|";
-  //         const formatedMemo = memo.replace(/\n/gi, "\n|");
-  //         res.send(`|memo\n|-${formatedMemo}|`);
-  //         res.send(
-  //           {
-  //             type: "stamp",
-  //             name: "writing_hand"
-  //           }
-  //         );
-  //       }
-  //       else {
-  //         res.send("@Ras Error at memo.js");
-  //       }
-  //     })
-  //   }
-  // })
+  robot.hear(/^(me|め|メ)(mo|も|モ)$/i, res => {
+    const { bot, name } = res.message.message.user;
+    if(!bot){
+      const qs = {user: name};
+      request.get(option(qs), (error,respond,body) => {
+        if(!error){
+          let { user, memo } = body;
+          if(memo == "") memo = "\n:404_notfound.ex-large:|";
+          const formatedMemo = memo.replace(/\n/gi, "\n|");
+          res.send(`|memo\n|-${formatedMemo}|`);
+          res.send(
+            {
+              type: "stamp",
+              name: "writing_hand"
+            }
+          );
+        }
+        else {
+          res.send("@Ras Error at memo.js");
+        }
+      })
+    }
+  })
 
   robot.hear(/^(me|め|メ)(mo|も|モ)(\=|＝)/i, res => {
     const { plainText, user } = res.message.message;
@@ -65,17 +65,16 @@ module.exports = robot => {
     }
   })
 
-  robot.hear(/^(me|め|メ)(mo|も|モ)(\+|＋)?(?!(=|＝))/i, res => {
+  robot.hear(/^(me|め|メ)(mo|も|モ)(\+|＋)z/i, res => {
     const { plainText, user } = res.message.message;
     const { bot, name } = user;
     if(!bot){
-      const i = plainText.search(/\n/);
-      if (i == -1) i = 4;
+      const i = plainText.search(/(\+|＋)/);
       const qs = {user: name};
       request.get(option(qs), (error,respond,body) => {
         if(!error){
           const { memo } = body;
-          const updatedMemo = memo + plainText.slice(i-1);
+          const updatedMemo = memo + plainText.slice(i + 1);
           const formatedMemo = updatedMemo.replace(/\n/gi, "\n|");
           const qs2 = {user: name, memo: updatedMemo};
           request.post(option(qs2), (error2,respond2,body2) => {
