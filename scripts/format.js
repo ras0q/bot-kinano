93b2c18f27beca8c018ca6839bc54ce7bce1bd67
Kira Kawai <asymptote.k.k@gmail.com>
2021-01-08 23:20:37 +0900

    feat fmt

---
+++ scripts/format.js
@@ -0,0 +1,25 @@
+module.exports = robot => {
+    robot.hear(/^(format|fmt)[ 　]matrix/i, res => {
+        const { user, plainText } = res.message.message;
+        if(!user.bot){
+            let text = plainText.slice(plainText.search(/\n/) + 1);
+            text = text.replace(/\n/gi, "\\\\").replace(/[ ,]/gi, "&");
+            res.send(`$$\n\\left(\\begin{array}{ccc}${text}\\end{array}\\right)\n$$`);
+        }
+    })
+
+    robot.hear(/^(format|fmt)[ 　]table/i, res => {
+        const { user, plainText } = res.message.message;
+        if(!user.bot){
+            let rows = plainText.slice(plainText.search(/\n/) + 1).split(/\n/);
+            const len = rows[0].match(/[,、， 　]/gi).length;
+            let table = rows[0].replace(/[,、， 　]/gi, "|") + "|\n";
+            for(let i = 0; i <= len; i++) table += "-|";
+            table += "\n";
+            for(let i = 1; i < rows.length; i++){
+                table += rows[i].replace(/[,、， 　]/gi, "|") + "|\n";
+            }
+            res.send(table);
+        }
+    })
+}
