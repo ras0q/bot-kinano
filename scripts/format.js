1d72bf199ca9362841f41c8b1235493a68cd57e5
Kira Kawai <asymptote.k.k@gmail.com>
2021-01-11 18:44:52 +0900

    fix

--- scripts/format.js
+++ scripts/format.js
@@ -3,21 +3,26 @@ module.exports = robot => {
         const { user, plainText } = res.message.message;
         if(!user.bot){
             let text = plainText.slice(plainText.search(/\n/) + 1);
-            text = text.replace(/\n/gi, "\\\\").replace(/[ ,]/gi, "&");
+            text = text.replace(/\n/gi, "\\\\").replace(/[,]/gi, "&");
             res.send(`$$\n\\left(\\begin{array}{ccc}${text}\\end{array}\\right)\n$$`);
         }
     })
 
     robot.hear(/^(format|fmt)[ 　]table/i, res => {
-        const { user, plainText } = res.message.message;
+        // const { user, plainText } = res.message.message;
+        const user = {"bot": false};
+        const plainText = "fmt table\nhoge\nhoge"
         if(!user.bot){
             let rows = plainText.slice(plainText.search(/\n/) + 1).split(/\n/);
-            const len = rows[0].match(/[,、， 　]/gi).length;
-            let table = rows[0].replace(/[,、， 　]/gi, "|") + "|\n";
+            let len = [], table = rows[0] + "|\n";
+            if(rows[0].search(/[,、，]/) != -1) {
+                len = rows[0].match(/[,、，]/gi).length;
+                table = rows[0].replace(/[,、，]/gi, "|") + "|\n";
+            }
             for(let i = 0; i <= len; i++) table += "-|";
             table += "\n";
             for(let i = 1; i < rows.length; i++){
-                table += rows[i].replace(/[,、， 　]/gi, "|") + "|\n";
+                table += rows[i].replace(/[,、，]/gi, "|") + "|\n";
             }
             res.send(table);
         }
