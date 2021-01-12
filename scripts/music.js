5a140aad9012cf38141cd939c2b327b1b72d596b
Shumaislime <lay82828@gmail.com>
2021-01-12 23:52:31 +0900

    refacter

--- scripts/music.js
+++ scripts/music.js
@@ -5,42 +5,38 @@ const request = require('request');
 //start以上end未満の乱数を返す
 const getRandom　= (start, end) => {
   return Math.floor(Math.random() * (end - start)) + start;
-}
+};
 
 //requestのoptionをつくる
 const option = (Q) => {
-  let op = {
+  return {
     uri: "https://script.google.com/macros/s/AKfycbwi70WcQuyozUl08tuQFrjiT7znHusgOURUXGigwFidHFFZrvkm/exec", //GAS
     headers: {'Content-type': 'application/json'},
     qs: Q,
     json: true
-  }
-  return op;
-}
+  };
+};
 
 //textからmusicとurlを抽出
 const extractValues = (text) => {
-  let values = {
+  const values = {
     music: "",
-    url: "",
-  }
-  const verticalIndex = text.indexOf("|"); //位置指定(!=-1のときエラー)
-  const newlineIndex = text.indexOf("\n"); //位置指定(!=-1のときエラー)
-  if(verticalIndex != -1 || newlineIndex != -1){
+    url: ""
+  };
+
+  if (text.includes("|") || text.includes("\n")) {
     return values;
   }
-  const kagikakko = text.indexOf("["); //URLがあれば読み取る
-  const kakko = text.indexOf("](");
-  const kakkotoji = text.indexOf(")");
-  if(kakko != -1 && kakkotoji != -1 && kagikakko != -1){
-    values.music = text.slice(kagikakko + 1, kakko); //曲名切り取り
-    values.url = text.slice(kakko + 2, kakkotoji);
-  }
-  else {
-    values.music = text.slice(5); //曲名切り取り
+
+  const music_element = text.match(/\[([^\[\]]*)\]\((.*)\)/);
+  if (music_element !== null){
+    values.music = music_element[1];
+    values.url = music_element[2];
+  } else {
+    values.music = text.replace(/^%add /i, ""); // 曲名切り取り
   }
   return values;
-}
+};
 
 /*module------------------------------------------------------------------*/
 
@@ -55,93 +51,94 @@ module.exports = robot => {
     if(!bot){
       const { music, url } = extractValues(plainText);
       const qs = {
-        "user": name,
-        "music": music,
-        "url": url
+        user: name,
+        music,
+        url
       };
       request.post(option(qs), (error, response, body) => {
         if(!error){
-          const addtable = `|User|Music|URL|\n|-|-|-|\n|:@${name}:${name}|${music}|${url}|\n`
-          res.send(`『${music}』を追加したやんね！\n${addtable}`)
+          const addtable = `|User|Music|URL|\n|-|-|-|\n|:@${name}:${name}|${music}|${url}|\n`;
+          res.send(`『${music}』を追加したやんね！\n${addtable}`);
           robot.send({channelID: logID},"## 曲が追加されたやんね！\n"+ addtable);
         }
-      })
+      });
     }
-  })
+  });
 
   //曲削除
   robot.hear(/^%remove .*/i, res => {
     if(!res.message.message.user.bot){
-      res.send(`@Ras 頼んだ！`)
+      res.send(`@Ras 頼んだ！`);
     }
-  })
+  });
 
     //曲確認
   robot.hear(/^%watch$/i, res => {
     if(!res.message.message.user.bot){
-      let table = "|No.|User|Music|\n|-:|:-:|-|\n|例|:kinano:|きなこもちもちのうた|\n"; //表の項目と例
       request.get(option(), (error, response, body) => {
         if(!error){
           //表作成
-          for(let i = 0;i < body.length; i++){
-            const { user, music } = body[i];
-            table += `|${i}|:@${user}:|${music}|\n`;
-          }
+          const table = [
+            "|No.|User|Music|",
+            "|-:|:-:|-|",
+            "|例|:kinano:|きなこもちもちのうた|", //表の項目と例
+            ...body.map(({ user, music }, idx) => `|${idx}|:@${user}:|${music}|`)
+            ]
+            .join("\n") + "\n";
           res.send(`## プレイリストやんね～\n${table}\n[](https://www.youtube.com/playlist?list=PLziwNdkdhnxiwuSjNF2k_-bvV1XojtWva)`);
         }
-      })
+      });
     }
-  })
+  });
 
   //曲確認(URLつき、番号指定)
   robot.hear(/^%watch [0-9]+/i, res => {
     if(!res.message.message.user.bot){
-      let table = "|No.|User|Music|URL|\n|-:|-|-|-|\n"; //表の項目と例
+      const tableExample = "|No.|User|Music|URL|\n|-:|-|-|-|"; //表の項目と例
       const { plainText } = res.message.message;
-      const i = plainText.slice(7);
+      const i = plainText.replace(/^%watch /i, "");
       request.get(option(), (error, response, body) => {
         if(!error){
           //表作成
           const { user, music, url } = body[i];
-          table = `${table}|${i}|:@${user}:${user}|${music}|${url}|\n`;
+          const table = `${tableExample}\n|${i}|:@${user}:${user}|${music}|${url}|\n`;
           res.send(`## 曲${i}はこれ！\n${table}`);
         }
-      })
+      });
     }
-  })
+  });
 
   //曲確認(URLつき、番号random)
-  robot.hear(/^%watch r/i, res => {
+  robot.hear(/^%watch r$/i, res => {
     if(!res.message.message.user.bot){
-      let table = "|No.|User|Music|URL|\n|-:|-|-|-|\n"; //表の項目と例
-      const { plainText } = res.message.message;
-      const i = plainText.slice(7);
+      const tableExample = "|No.|User|Music|URL|\n|-:|-|-|-|"; //表の項目と例
       request.get(option(), (error, response, body) => {
         if(!error){
           //表作成
-          const i = getRandom(0,body.length);
+          const i = getRandom(0, body.length);
           const { user, music, url } = body[i];
-          table = `${table}|${i}|:@${user}:${user}|${music}|${url}|\n`;
+          const table = `${tableExample}\n|${i}|:@${user}:${user}|${music}|${url}|\n`;
           res.send(`## きなののオススメソングはこれ！\n${table}`);
         }
-      })
+      });
     }
-  })
+  });
 
   //曲確認(URLつき、全部)
   robot.hear(/^%watch all$/i, res => {
     if(!res.message.message.user.bot){
-      let table = "|No.|User|Music|URL|\n|-:|-|-|-|\n||:kinano:BOT_kinano|きなこもちもちのうた|https://wiki.trap.jp/bot/kinano|\n"; //表の項目と例
       request.get(option(), (error, response, body) => {
         if(!error){
           //表作成
-          for(let i = 0;i < body.length; i++){
-            const { user, music, url } = body[i];
-            table = `${table}|${i}|:@${user}:${user}|${music}|${url}|\n`;
-          }
+          const table = [
+            "|No.|User|Music|URL|",
+            "|-:|-|-|-|",
+            "||:kinano:BOT_kinano|きなこもちもちのうた|https://wiki.trap.jp/bot/kinano|", //表の項目と例
+            ...body.map(({ user, music, url }, idx) => `|${idx}|:@${user}:${user}|${music}|${url}|`)
+          ].join("\n") + "\n";
           res.send(`## プレイリストやんね～\n${table}`);
         }
-      })
+      });
     }
-  })
-}
+  });
+};
