5a19280c03901c97563a58fac924fb23f1116837
Kira Kawai <asymptote.k.k@gmail.com>
2021-02-01 21:30:57 +0900

    ちょっとだけ整理

--- scripts/command/memo.js
+++ scripts/command/memo.js
@@ -5,7 +5,6 @@ require('dotenv').config();
 const request = require('request');
 const cron = require('node-cron');
 
-//requestのoptionをつくる
 const option = (url, Q = {}) => ({
   uri: url,
   headers: {'Content-type': 'application/json'},
@@ -13,83 +12,65 @@ const option = (url, Q = {}) => ({
   json: true
 });
 
-const url = process.env.SHOWCASE_URL+ '/memo';
+const format = (memo) => (
+  memo !== ''
+    ? memo.replace(/\n/gi, '|\n|')
+    : '\n|:404_notfound.ex-large:|'
+);
+
+const gtR_ID ='f58c72a4-14f0-423c-9259-dbb4a90ca35f';
 const clientID = process.env.SHOWCASE_CLIENT_ID;
+const url = process.env.SHOWCASE_URL+ '/memo';
 
 module.exports = robot => {
-  const gtR_ID ='f58c72a4-14f0-423c-9259-dbb4a90ca35f';
   robot.hear(/^(me|め|メ)(mo|も|モ)$/i, res => {
     const { bot, name } = res.message.message.user;
     if(!bot){
       request.get(option(`${url}/${name}?client_id=${clientID}`), (error,respond,body) => {
-        if(!error){
-          const { memo } = body;
-          const formatedMemo = memo !== ''
-            ? memo.replace(/\n/gi, '\n|')
-            : '\n|:404_notfound.ex-large:|';
-          res.send(`|memo\n|-${formatedMemo}|`);
-          res.send(
-            {
-              type: 'stamp',
-              name: 'writing_hand'
-            }
-          );
+        if(error){
+          res.send('@Ras Error at memo.js: ' + error.toString());
         }
         else {
-          res.send('@Ras Error at memo.js: ' + error.toString());
+          const { memo } = body;
+          res.send(`|memo\n|-|\n${format(memo)}|`);
+          res.send({ type: 'stamp', name: 'writing_hand' });
         }
       });
     }
   });
 
-  robot.hear(/^(me|め|メ)(mo|も|モ)(=|＝)+/i, res => {
+  robot.hear(/^(me|め|メ)(mo|も|モ)(=|＝)\s?/i, res => {
     const { text, user } = res.message.message;
     const { bot, name } = user;
     if(!bot){
-      const memo = text.replace(/^(me|め|メ)(mo|も|モ)(=|＝|\s)+/i, '');
+      const memo = text.replace(/^(me|め|メ)(mo|も|モ)(=|＝)\s?/i, '');
       const Q = {user: name, memo};
       request.post(option(`${url}?client_id=${clientID}`, Q), (error, _respond, _body) => {
-        if(!error){
-          const formatedMemo = memo !== ''
-            ? memo.replace(/\n/gi, '|\n|')
-            : '|:404_notfound.ex-large:|';
-          res.send(`|memo|\n|-|\n${formatedMemo}|`);
-          res.send(
-            {
-              type: 'stamp',
-              name: 'writing_hand'
-            }
-          );
+        if(error){
+          res.send('@Ras Error at memo.js: ' + error.toString());
         }
         else{
-          res.send('@Ras Error at memo.js: ' + error.toString());
+          res.send(`|memo|\n|-|\n${format(memo)}|`);
+          res.send({type: 'stamp', name: 'writing_hand'});
         }
       });
     }
   });
 
-  robot.hear(/^(me|め|メ)(mo|も|モ)(\+|＋|\s)*\n/i, res => {
+  robot.hear(/^(me|め|メ)(mo|も|モ)(\+|＋)\s?/i, res => {
     const { text, user } = res.message.message;
     const { bot, name } = user;
     if(!bot){
-      const memo = text.replace(/^(me|め|メ)(mo|も|モ)(\+|＋|\s)*/i, '\n');
+      const memo = text.replace(/^(me|め|メ)(mo|も|モ)(\+|＋)\s?/i, '');
       const qs = {user: name, memo};
       request.patch(option(`${url}?client_id=${clientID}`, qs), (error, respond, body) => {
-        if(!error){
-          const { memo } = body;
-          const formatedMemo = memo !== ''
-            ? memo.replace(/\n/gi, '|\n|')
-            : '\n|:404_notfound.ex-large:|';
-          res.send(`|memo|\n|-${formatedMemo}|`);
-          res.send(
-            {
-              type: 'stamp',
-              name: 'writing_hand'
-            }
-          );
+        if(error){
+          res.send('@Ras Error at memo.js: ' + error.toString());
         }
         else {
-          res.send('@Ras Error at memo.js: ' + error.toString());
+          const { memo } = body;
+          res.send(`|memo|\n|-|\n${format(memo)}|`);
+          res.send({type: 'stamp', name: 'writing_hand'});
         }
       });
     }
@@ -100,8 +81,7 @@ module.exports = robot => {
       if(!error){
         const { memo } = body;
         if(memo){
-          const formatedMemo = memo.replace(/\n/gi, '\n|');
-          robot.send({channelID: gtR_ID}, `|memo\n|-${formatedMemo}|`);
+          robot.send({channelID: gtR_ID}, `|memo\n|-${format(memo)}|`);
         }
       }
     });
--- scripts/command/test.js
+++ scripts/command/test.js
@@ -1,10 +1,18 @@
 //Description:
 // work in progress.
 
+const gtRB_ID = '2a5616d5-5d69-4716-8377-1e1fb33278fe';
+
 module.exports = robot => {
   // sample
   robot.hear(/fuga/, res => {
     res.send('fugafuga');
+  });
+
+  robot.respond(/info/, res => {
+    if(res.message.message.channelId === gtRB_ID) {
+      res.send(`\`\`\`\n${JSON.stringify(res.message)}\n\`\`\``);
+    }
   });
 
   robot.hear(/.+[食た]べたい$/, res => {
