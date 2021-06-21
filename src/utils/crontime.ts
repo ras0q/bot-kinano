8d9afed7075349ad870bc476a155c4a9527c04eb
Ras96 <asymptote.k.k@gmail.com>
2021-06-21 17:53:04 +0900

    :bug: cron直した

--- src/utils/crontime.ts
+++ src/utils/crontime.ts
@@ -10,8 +10,5 @@ export const convertToCronTime = (date: Date): string => {
 export const convertToCronTimePerDate = (date: Date): string => {
   const m = date.getMinutes();
   const h = date.getHours();
-  const d = '*';
-  const M = date.getMonth();
-  const D = date.getDay();
-  return `${m} ${h} ${d} ${M + 1} ${D}`;
+  return `${m} ${h} * * *`;
 };
