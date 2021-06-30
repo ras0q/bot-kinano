cf5a2c6bfec0531fec4019d4b1dc15d477355532
Ras96 <asymptote.k.k@gmail.com>
2021-06-30 09:27:03 +0900

    ♻️  fetchに
    URLを環境変数にした

--- src/command/translate.ts
+++ src/command/translate.ts
@@ -1,64 +1,60 @@
 //Description:
 //  translate words.
 
-import rp from 'request-promise';
+import fetch from 'node-fetch';
 import { Robots } from '../src/types';
 import { IDs } from '../src/words';
 
-const op = (txt: string, src: string, tar: string) => ({
-  uri:
-    'https://script.google.com/macros/s/AKfycby3sNNJlWubQB4q_K4xHjZnpCxaCtYzbcuPmT-r9PJGTs4ZMb0/exec',
-  headers: { 'Content-type': 'application/json' },
-  qs: {
-    text: txt,
-    source: src,
-    target: tar,
-  },
-  json: true,
-});
+const baseApiUrl = `${process.env.GAS_TRA_URL}`;
+const apiUrl = (txt: string, src: string, tar: string) => new URL(`${baseApiUrl}?text="${txt}"&source=${src}&target=${tar}`);
 
 module.exports = (robot: Robots) => {
   //翻訳(デフォルトは日=>英)
-  robot.hear(/^tra\s+/i, (res) => {
+  robot.hear(/^tra\s+/i, async (res) => {
     const { id, plainText, user } = res.message.message;
     if (!user.bot) {
       const txt = plainText.replace(/^tra\s+/i, '');
       const langs = plainText.match(/[([]\s*(..)\s*([=-]>|→)\s*(..)\s*[)\]]/);
       const [src, tar] = langs !== null ? [langs[1], langs[3]] : ['ja', 'en'];
-      rp(op(txt, src, tar))
-        .then((body) => {
-          if (body.text) res.reply(body.text);
-          else res.reply('きなのその言葉知らない！');
-        })
-        .catch((err) => {
-          console.log(err);
-          robot.send(
-            { userID: IDs['@Ras'] },
-            `${err}\nhttps://q.trap.jp/messages/${id}`
-          );
-        });
+
+      try {
+        const body = await fetch(apiUrl(txt, src, tar));
+        if(body.status !== 200) throw new Error(body.statusText);
+        const { text } = await body.json();
+        if(text !== '') res.reply(text);
+        else res.reply('きなのその言葉知らない！');
+      } catch (err) {
+        console.log(err);
+        robot.send(
+          { userID: IDs['@Ras'] },
+          `${err}\nhttps://q.trap.jp/messages/${id}`
+        );
+      }
     }
   });
 
   //逆翻訳
-  robot.hear(/^tratra\s+/i, (res) => {
+  robot.hear(/^tratra\s+/i, async (res) => {
     const { id, plainText, user } = res.message.message;
     if (!user.bot) {
       const txt = plainText.replace(/^tratra\s+/, '');
-      rp(op(txt, '', 'en'))
-        .then((body) => {
-          return rp(op(body.text, '', 'ja'));
-        })
-        .then((body2) => {
-          res.reply(body2.text);
-        })
-        .catch((err) => {
-          console.log(err);
-          robot.send(
-            { userID: IDs['@Ras'] },
-            `${err}\nhttps://q.trap.jp/messages/${id}`
-          );
-        });
+
+      try {
+        const body = await fetch(apiUrl(txt, 'ja', 'en'));
+        if(body.status !== 200) throw new Error(body.statusText);
+        const { text } = await body.json();
+        const body2 = await fetch(apiUrl(text, 'en', 'ja'));
+        if(body2.status !== 200) throw new Error(body.statusText);
+        const { text: text2 } = await body2.json();
+        if(text2 !== '') res.reply(text2);
+        else res.reply('きなのその言葉知らない！');
+      } catch (err) {
+        console.log(err);
+        robot.send(
+          { userID: IDs['@Ras'] },
+          `${err}\nhttps://q.trap.jp/messages/${id}`
+        );
+      }
     }
   });
 };
