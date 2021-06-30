b46aea2e3106e736c7859410fc1ba5623533fc52
Ras96 <asymptote.k.k@gmail.com>
2021-06-30 09:42:42 +0900

    ♻️  event.tsでfetchを使う

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -1,5 +1,5 @@
 import ical from 'ical';
-import requestPromise from 'request-promise';
+import fetch from 'node-fetch';
 import cron from 'node-cron';
 import { IDs } from '../src/words';
 import { Robots } from '../src/types';
@@ -13,48 +13,46 @@ if (!baseURL) {
 }
 
 /* eslint @typescript-eslint/no-non-null-assertion: 0 */
-export const setTodayEvents = (robot: Robots): void => {
+export const setTodayEvents = async (robot: Robots): Promise<void> => {
   console.log('Start setting today\'s events');
   for (const event of events) {
     event.destroy();
   }
   events.splice(0);
 
-  requestPromise(baseURL)
-    .then((body) => {
-      const ics = ical.parseICS(body);
-      const sortedKeys = Object.keys(ics).sort((a, b) => {
-        if (ics[a].start! < ics[b].start!) return -1;
-        else return 1;
-      });
-      const now = Date.now();
-      const todayEventKeys = sortedKeys.filter(
-        (key) =>
-          ics[key].start &&
+  try {
+    const body = await fetch(baseURL);
+    const ics = ical.parseICS(await body.text());
+    const sortedKeys = Object.keys(ics).sort((a, b) => {
+      if (ics[a].start! < ics[b].start!) return -1;
+      else return 1;
+    });
+    const now = Date.now();
+    const todayEventKeys = sortedKeys.filter(
+      (key) =>
+        ics[key].start &&
           ics[key].start!.getTime() - now > 0 &&
           ics[key].start!.getTime() - now < 1000 * 60 * 60 * 24
+    );
+    console.log(`${todayEventKeys.length} events found.`);
+    todayEventKeys.forEach((key) => {
+      const { start, summary, description } = ics[key];
+      const notifyTime = new Date(start!.getTime());
+      notifyTime.setMinutes(notifyTime.getMinutes() - 5);
+      events.push(
+        cron.schedule(
+          convertToCronTime(notifyTime),
+          () => {
+            robot.send(
+              { channelID: IDs['#g/t/Ras'] },
+              `# ==『${summary}』==\n#### ${start!.toString()}\n${description}\n@Ras`
+            );
+          },
+          { timezone: 'Asia/Tokyo' }
+        )
       );
-      console.log(`${todayEventKeys.length} events found.`);
-      todayEventKeys.forEach((key) => {
-        const { start, summary, description } = ics[key];
-        const notifyTime = new Date(start!.getTime());
-        notifyTime.setMinutes(notifyTime.getMinutes() - 5);
-        events.push(
-          cron.schedule(
-            convertToCronTime(notifyTime),
-            () => {
-              robot.send(
-                { channelID: IDs['#g/t/Ras'] },
-                `# ==『${summary}』==\n#### ${start!.toString()}\n${description}\n@Ras`
-              );
-            },
-            { timezone: 'Asia/Tokyo' }
-          )
-        );
-      });
-    })
-    .catch((err) => {
-      robot.send({ userID: IDs['@Ras'] }, err);
-      return err;
     });
+  } catch (err) {
+    robot.send({ userID: IDs['@Ras'] }, `${err}`);
+  }
 };
