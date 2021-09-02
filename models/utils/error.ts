a2583bfae3c74a5dd7a407f645d5398d7f461c5e
Ras96 <asymptote.k.k@gmail.com>
2021-09-03 02:14:18 +0900

    ♻️ Update Error Message

--- models/utils/error.ts
+++ models/utils/error.ts
@@ -1,10 +1,12 @@
 import { defaultEnvs } from '../config/env'
 import { IDs } from '../config/id'
 import { apiPostDM } from '../src/traqapi'
+import { RobotHearResponse } from '../src/types'
 
 const { baseUrl } = defaultEnvs.traq
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types,@typescript-eslint/no-explicit-any
-export const postErrMsg = (err: any, msgId: string) => {
-  apiPostDM(IDs['@Ras'], `${err}\n${baseUrl}/${msgId}`)
+export const postErrMsg = (err: any, res: RobotHearResponse) => {
+  console.error(err)
+  apiPostDM(IDs['@Ras'], `${err}\n${baseUrl}/${res.message.message.id}`)
 }
