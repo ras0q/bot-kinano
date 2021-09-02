bff07b9d29db00e43d1579d512f2719a41df27d5
Ras96 <asymptote.k.k@gmail.com>
2021-09-02 23:00:39 +0900

    ♻️ ディレクトリ構造の変更

--- models/src/words.ts
+++ models/src/words.ts
@@ -2,7 +2,7 @@ import path from 'path'
 import fs from 'fs'
 
 export const readme = fs.readFileSync(
-  path.resolve(__dirname, './help.md'),
+  path.resolve(__dirname, '../../help.md'),
   'utf8'
 )
 
